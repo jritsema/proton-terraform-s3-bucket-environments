b458e1b64f49a6afa9efac55b760796f164c8eaa
John Ritsema <john_ritsema@yahoo.com>
2022-08-25 17:50:47 -0400

    create new oidc provider vs looking it up

--- setup/data.tf
+++ setup/data.tf
@@ -3,7 +3,3 @@ data "aws_caller_identity" "current" {}
 data "tls_certificate" "github_actions_oidc_provider" {
   url = "https://token.actions.githubusercontent.com/.well-known/openid-configuration"
 }
-
-data "aws_iam_openid_connect_provider" "github" {
-  url = "https://token.actions.githubusercontent.com"
-}
--- setup/main.tf
+++ setup/main.tf
@@ -1,10 +1,17 @@
+# This identity provider is required to accept OpenID Connect credentials
+resource "aws_iam_openid_connect_provider" "github" {
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.github_actions_oidc_provider.certificates[0].sha1_fingerprint]
+}
+
 # the role that the github action runs as
 resource "aws_iam_role" "github_actions" {
   name               = var.namespace
   assume_role_policy = data.aws_iam_policy_document.github_actions_assume_role.json
 
   inline_policy {
-    name = "proton"
+    name = "github-action-policy"
 
     policy = jsonencode({
       Statement = [
@@ -21,10 +28,12 @@ resource "aws_iam_role" "github_actions" {
 data "aws_iam_policy_document" "github_actions_assume_role" {
   statement {
     actions = ["sts:AssumeRoleWithWebIdentity"]
+
     principals {
       type        = "Federated"
-      identifiers = [data.aws_iam_openid_connect_provider.github.arn]
+      identifiers = [aws_iam_openid_connect_provider.github.arn]
     }
+
     condition {
       test     = "StringLike"
       variable = "token.actions.githubusercontent.com:sub"
